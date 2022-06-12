ce7ddc0edfa160fe17ff651802445f8de3ead1fa
zyperr <103399102+zyperr@users.noreply.github.com>
2022-06-11 21:48:28 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,39 +1,39 @@
 // Setup End Date for Countdown (getTime == Time in Milleseconds)
 let launchDate = new Date("julio 01, 2022 03:00:00").getTime();
 
-// Setup Timer to tick every 1 second
+// un tick cada segundo 
 let timer = setInterval(tick, 1000);
 
 function tick () {
-  // Get current time
+  // Tomar el tiempo actual
 let now = new Date().getTime();
   // Get the difference in time to get time left until reaches 0
 let t = launchDate - now;
 
-  // Check if time is above 0
+  // verificar si esta por arriba de 0
 if (t > 0) {
-    // Setup Days, hours, seconds and minutes
-    // Algorithm to calculate days...
+    //  Days, hours, seconds and minutes
+    // calculo para days
     let days = Math.floor(t / (1000 * 60 * 60 * 24));
     // prefix any number below 10 with a "0" E.g. 1 = 01
     if (days < 10) { days = "0" + days; }
     
-    // Algorithm to calculate hours
+    // calculo para hours
     let hours = Math.floor((t % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
     if (hours < 10) { hours = "0" + hours; }
 
-    // Algorithm to calculate minutes
+    // calculo para  minutes
     let mins = Math.floor((t % (1000 * 60 * 60)) / (1000 * 60));
     if (mins < 10) { mins = "0" + mins; }
 
-    // Algorithm to calc seconds
+    // calculo para seconds
     let secs = Math.floor((t % (1000 * 60)) / 1000);
     if (secs < 10) { secs = "0" + secs; }
 
-    // Create Time String
+    // String para el tiempo 
     let time = `${days} : ${hours} : ${mins} : ${secs}`;
 
-    // Set time on document
+    // mostrar el contador en document con la clase .coutdown
     document.querySelector('.countdown').innerText = time;
 }
 }
